7428a11123b945367aa5334ae9e30d12033fc861
Vivek Kumar <ervivekkumar677@gmail.com>
2025-12-19 06:38:34 +0530

    .env file updated

--- backend/src/services/certificate.service.js
+++ backend/src/services/certificate.service.js
@@ -61,7 +61,13 @@ export async function generateCertificate(data) {
 
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--single-process"
+    ]
   });
 
   const page = await browser.newPage();
