c1e815715d8e54b33bd700e35243dbad0a877770
Vivek Kumar <ervivekkumar677@gmail.com>
2025-12-22 01:17:01 +0530

    changes made

--- backend/src/services/launchBrowser.service.js
+++ backend/src/services/launchBrowser.service.js
@@ -1,9 +1,8 @@
 import puppeteer from "puppeteer";
 
 export const launchBrowser = async () => {
-  const browser = await puppeteer.launch({
-    executablePath:
-      process.env.CHROME_PATH || puppeteer.executablePath(), // <-- key fix
+    console.log("Puppeteer executable:", puppeteer.executablePath());
+    const browser = await puppeteer.launch({
     headless: true,
     args: [
       "--no-sandbox",
